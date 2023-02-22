3e6bcb2790a9c7cca5ae02ead758562be22860e8
Brian White <devbrianwhite@gmail.com>
2023-02-21 22:37:11 -0500

    updated routes

--- app.js
+++ app.js
@@ -3,24 +3,59 @@ const app = express()
 const port = 3000
 require('dotenv').config()
 const mysql = require('mysql2')
-const connection = mysql.createConnection(process.env.DATABASE_URL)
-console.log('Connected to PlanetScale!')
-connection.end()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.post('/api/add', (req, res) => {
+    api_route = Buffer.from(Math.random().toString()).toString("base64").substring(10,20);
 
-app.post('/api/new', (req, res) => {
-    res.send('Hello World!')
+    let cnxn = mysql.createConnection(process.env.DATABASE_URL)
+
+    cnxn.execute('insert into briantools.yestful_routes (api_route, http_resp_body, user_id) values (?, ?, ?)', [api_route, req.resp_body, req.user], function(err, results, fields) {
+      if(results.affectedRows > 0) {
+        res.send('Route Added')
+      } else {
+        res.send('Route Not Added')
+      }
+    })
+    cnxn.end()
 })
 
 app.get('/api/:routeKey', (req, res) => {
-    res.send('Hello World!')
+  if (typeof req.params.routeKey === undefined) {
+    res.send('Route Missing')
+    return
+  }
+
+  let cnxn = mysql.createConnection(process.env.DATABASE_URL)
+
+  cnxn.query("select http_resp_body from briantools.yestful_routes where api_route = ?", [req.params.routeKey], function(err, results) {
+
+    if(typeof results[0]['http_resp_body'] !== undefined) {
+      res.send(results[0]['http_resp_body'])
+    } else {
+      res.send('Route Not Found')
+    }
+
+  })
+  cnxn.end()
 })
 
 app.get('/api/:routeKey/delete', (req, res) => {
-    res.send('Hello World!')
+  if (typeof req.params.routeKey === undefined) {
+    res.send('Route Missing')
+    return
+  }
+
+  let cnxn = mysql.createConnection(process.env.DATABASE_URL)
+
+  cnxn.execute('update briantools.yestful_routes set active_status = "D" where api_route = ?', [req.params.routeKey], function(err, results, fields) {
+    if(results.affectedRows > 0) {
+      res.send('Route Deleted')
+    } else {
+      res.send('Route Not Found')
+    }
+  })
+  cnxn.end()
+
 })
 
 app.listen(port, () => {
